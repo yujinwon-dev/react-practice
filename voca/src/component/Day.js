a71d894ca9fdb1f25aaf8b4ac5ac0799421dbd23
yujin <wwonyujin@gmail.com>
2022-05-28 22:38:38 +0900

    feat: 🎸 api 호출 추가

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,21 +1,27 @@
 import dummy from '../db/data.json';
 import { useParams } from 'react-router-dom';
+import Word from './Word';
+import { useEffect, useState } from 'react';
 
 export default function Day() {
   // const day = 1;
   const { day } = useParams();
-  const wordList = dummy.words.filter(word => word.day === Number(day));
+  // const wordList = dummy.words.filter(word => word.day === Number(day));
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+      .then(res => res.json())
+      .then(data =>setWords(data));
+  }, [day]);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word => (
-            <tr key={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+          {words.map(word => (
+            <Word key={word.id} word={word} />
           ))}
         </tbody>
       </table>
--- voca/src/component/DayList.js
+++ voca/src/component/DayList.js
@@ -1,11 +1,18 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import dummy from '../db/data.json';
 
 export default function DayList() {
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/days')
+      .then(res => res.json())
+      .then(data => setDays(data));
+  }, []);
 
   return (
     <ul>
-      {dummy.days.map(day => (
+      {days.map(day => (
         <li key={day.id}>
           {/* Link는 a 태그로 렌더링됨 */}
           <Link to={`/day/${day.day}`}>
