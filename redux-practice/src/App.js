adf4f3018db801e2c021b5115d0bf56f8b5c3361
yujin <wwonyujin@gmail.com>
2022-06-13 23:23:58 +0900

    feat: 🎸 redux 적용

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,67 +1,86 @@
-import { useState } from "react";
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider, useSelector, useDispatch, connect } from 'react-redux';
+
+function reducer(currentState, action) {
+  if (currentState === undefined) {
+    return { number: 1};
+  }
+  const newState = {...currentState}; // 복제본을 이용해서 불변성 유지
+  // useDispatch에서 작성한 type을 여기서 사용
+  if (action.type === 'PLUS') {
+    newState.number++;
+  }
+  return newState;
+}
+const store = configureStore({ reducer });
 
 export default function App() {
-  const [number, setNumber] = useState(1);
   return (
     <div id="container">
-      <h1>Root: {number}</h1>
+      <h1>Root</h1>
       <div id="grid">
-        <Left1 number={number} />
-        <Right1 onIncrease={() => setNumber(number + 1)} />
+        <Provider store={store}>
+          <Left1 />
+          <Right1 />
+        </Provider>
       </div>
     </div>
   );
 }
 
-function Left1({ number }) {
+function Left1() {
   return (
     <div>
-      <h1>Left1 : {number}</h1>
-      <Left2 number={number} />
+      <h1>Left1</h1>
+      <Left2/>
     </div>
   );
 }
 
-function Left2({ number }) {
+function Left2() {
+  console.log('Left2');
   return (
     <div>
-      <h1>Left2 : {number}</h1>
-      <Left3 number={number} />
+      <h1>Left2</h1>
+      <Left3 />
     </div>
   );
 }
 
-function Left3({ number }) {
+function Left3() {
+  console.log('Left3');
+  const number = useSelector(state => state.number);
   return (
     <div>
-      <h1>Left3 : {number}</h1>
+      <h1>Left3: {number}</h1>
     </div>
   );
 }
 
-function Right1({ onIncrease }) {
+function Right1() {
   return (
     <div>
       <h1>Right1</h1>
-      <Right2 onIncrease={onIncrease} />
+      <Right2 />
     </div>
   );
 }
 
-function Right2({ onIncrease }) {
+function Right2() {
   return (
     <div>
       <h1>Right2</h1>
-      <Right3 onIncrease={onIncrease} />
+      <Right3 />
     </div>
   );
 }
 
-function Right3({ onIncrease }) {
+function Right3() {
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Right3</h1>
-      <button type="button" onClick={onIncrease}>+</button>
+      <button type="button" onClick={() => dispatch({ type: 'PLUS' })}>+</button>
     </div>
   );
 }
